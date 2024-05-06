b90b560da6b15491563a939767a48db805ec46c9
wusel <wusel+src@uu.org>
2024-05-07 00:55:15 +0200

    Change to "export only prefixes listed in our icvpn-meta file"

--- icvpn/templates/gen_icvpn_pfx_file.sh
+++ icvpn/templates/gen_icvpn_pfx_file.sh
@@ -1,23 +1,8 @@
 #!/bin/sh -e
 # {{ ansible_managed }}
-DATADIR=/opt/icvpn-meta
-
-cd /etc/tinc/icvpn
-git pull -q
-
-cd "$DATADIR"
-git pull -q
-sudo -u nobody /opt/icvpn-scripts/mkbgp -4 -s "$DATADIR" -x {{ icvpn_communityname }} -d icvpn_t -p icvpn_ > /etc/bird/bird.conf.d/icvpn.conf
-sudo -u nobody /opt/icvpn-scripts/mkbgp -6 -s "$DATADIR" -x {{ icvpn_communityname }} -d icvpn_t -p icvpn_ > /etc/bird/bird6.conf.d/icvpn.conf
 
 (PATH=$PATH:/root/.local/bin ; export PATH ; cat /opt/icvpn-meta/{{ icvpn_communityname }} | shyaml  get-value networks.ipv6 | awk '{pfx[numpfx++]=$NF;} END{printf("LOCAL_ICVPN_PFX = [ "); if(numpfx>0) {for(i=0; i<numpfx; i++) {printf("%s%s", pfx[i], i<numpfx-1?", ":"");}} printf(" ];\n");}' >/etc/bird/bird6.conf.d/LOCAL_ICVPN_PFX_ ; mv /etc/bird/bird6.conf.d/LOCAL_ICVPN_PFX_ /etc/bird/bird6.conf.d/LOCAL_ICVPN_PFX)
 (PATH=$PATH:/root/.local/bin ; export PATH ; cat /opt/icvpn-meta/{{ icvpn_communityname }} | shyaml  get-value networks.ipv4 | awk '{pfx[numpfx++]=$NF;} END{printf("LOCAL_ICVPN_PFX = [ "); if(numpfx>0) {for(i=0; i<numpfx; i++) {printf("%s%s", pfx[i], i<numpfx-1?", ":"");}} printf(" ];\n");}' >/etc/bird/bird.conf.d/LOCAL_ICVPN_PFX_ ; mv /etc/bird/bird.conf.d/LOCAL_ICVPN_PFX_ /etc/bird/bird.conf.d/LOCAL_ICVPN_PFX)
 
-birdc configure > /dev/null
-birdc6 configure > /dev/null
-
-# TODO: This doesn't belong here but in the unbound role
-#sudo -u nobody /opt/icvpn-scripts/mkdns -s "$DATADIR" -x {{ icvpn_communityname }} -f unbound > /etc/unbound/unbound.conf.d/icvpn.conf
-#if unbound-checkconf /etc/unbound/unbound.conf.d/icvpn.conf >/dev/null 2>&1; then
-#  unbound-control reload > /dev/null
-#fi
+birdc configure > /dev/null ||:
+birdc6 configure > /dev/null ||:
