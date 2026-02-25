c1891c7695bf2c5ceb821dbde20465ef8223c244
wusel <wusel+src@uu.org>
2026-02-25 23:27:39 +0100

    Fiy yarn repo shit

--- gateways_hopglass-server/files/fix_data.sh
+++ gateways_hopglass-server/files/fix_data.sh
@@ -11,4 +11,4 @@ REQ_URI="$(echo $REQUEST_URI | sed -e 's%^/json%%g')"
 
 logger "$0: http://127.0.0.1:4444$REQ_URI"
 
-wget -O - -q "http://127.0.0.1:4444$REQ_URI" | jq '(.nodes[] | select(.nodeinfo.owner.contact?) | .nodeinfo.owner.contact) |= "none-of-your-business.ffmuc@mail.local"'
+wget -O - -q "http://127.0.0.1:4444$REQ_URI" | jq '(.nodes[] | select(.nodeinfo.owner.contact?) | .nodeinfo.owner.contact) |= "none-of-your-business.ffmuc@mail.google"'
