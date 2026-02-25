0fbd4abc35dd6c3a6417bfaca67fd3f069c8285e
Kai 'wusel' Siering <wusel+src@uu.org>
2026-02-25 19:25:15 +0100

    Depublish contact data

---
+++ gateways_hopglass-server/files/fix_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# HTTP-Header ausgeben
+echo "Content-Type: application/json"
+echo
+
+# Query-String verfügbar als:
+# $QUERY_STRING  -> filter=bar&value=barfoo
+
+REQ_URI="$(echo $REQUEST_URI | sed -e 's%^/json%%g')"
+
+logger "$0: http://127.0.0.1:4000$REQ_URI" 
+
+wget -O - -q "http://127.0.0.1:4000$REQ_URI" | jq '(.nodes[] | select(.nodeinfo.owner.contact?) | .nodeinfo.owner.contact) |= "none.of.your@business.ffmuc.net"'
